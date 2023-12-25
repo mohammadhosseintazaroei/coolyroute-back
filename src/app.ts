3feefb4ff4a19a92a7d4c90748de87495cb353fb
mohammad753 <m.h.tazaroie753@gmail.com>
2023-12-26 00:08:47 +0330

    change project structure

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AuthController } from './Authentication/controller/auth.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AuthController],
   providers: [AppService],
 })
 export class AppModule {}
