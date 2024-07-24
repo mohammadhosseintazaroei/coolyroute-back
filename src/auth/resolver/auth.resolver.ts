a143b9f89c6eda246f02092e84ffabc847eafc9d
mohammad753 <m.h.tazaroie753@gmail.com>
2024-07-24 14:37:57 +0330

    add check-auth resolver

--- src/auth/resolver/auth.resolver.ts
+++ src/auth/resolver/auth.resolver.ts
@@ -2,11 +2,18 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { AuthService } from '../auth.service';
 import { LoginDto, VerifyOtp } from '../dto/auth.dto';
 import { LoginVerification, UserVeify } from '../models/auth.model';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../jwt-auth.guard';
 
 @Resolver('Auth')
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Query(() => String)
+  async checkAuth() {
+    return 'success';
+  }
   @Query(() => LoginVerification)
   async login(@Args('user') user: LoginDto): Promise<LoginVerification> {
     return await this.authService.login(user);
