1b72d2062f3ed7f2d9ba08eded7ca3b0a2d20cfc
mohammad753 <m.h.tazaroie753@gmail.com>
2024-07-27 23:11:44 +0330

    create getSkillsBrief resolver

--- src/skill/skill.module.ts
+++ src/skill/skill.module.ts
@@ -4,12 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SkillEntity } from './entities/skill.entity';
 import { SkillService } from './skill.service';
 import { SkillCategoryEntity } from './entities/skill-category';
+import { SkillResolver } from './resolver/skill.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([SkillEntity, SkillCategoryEntity]),
     ConfigModule,
   ],
-  providers: [SkillService],
+  providers: [SkillResolver, SkillService],
 })
 export class SkillModule {}
